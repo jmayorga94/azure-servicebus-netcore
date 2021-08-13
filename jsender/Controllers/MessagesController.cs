3ca4090a39d9639635ab72278bce10998b317709
javiermayorga <javi.mayorga1994@outlook.com>
2021-08-13 16:52:19 -0600

    Initial commit creating proyect base

---
+++ jsender/Controllers/MessagesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using jsender.Application;
+using jsender.Requests;
+using jsender.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace jsender.Controllers
+{
+  
+    [Route("api/messages")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+
+        private readonly IMessageService _messageService;
+
+        public MessagesController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        [HttpPost("send")]
+        public async Task<ActionResult> Send (SendMessageRequest request)
+        {
+            try
+            {
+                 await _messageService.SendMessageAsync(request.Message);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                SendMessageResponse response = new SendMessageResponse()
+                {
+                    Code = 500,
+                    Message = ex.Message,
+                    Description = "Internal Server Error"
+
+                };
+
+                return StatusCode(response.Code, response);
+
+            }
+        }
+
+        [HttpGet("version")]
+        public ActionResult GetVersion()
+        {
+
+            return Ok("v1");
+        }
+    }
+}
---
+++ jsender/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using jsender.Application;
+using jsender.Services;
+using jsender.Utility;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace jsender
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddScoped<IMessageService, AzServiceBusMessageService>();
+            services.Configure<AzServiceBusConfig>(Configuration.GetSection("AzServiceBusConfig"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
