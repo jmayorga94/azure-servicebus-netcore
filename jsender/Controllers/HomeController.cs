b0a82ed4dc19c9657b9a0c4bb3f7c27ea93d3c42
jmayorga94 <javi.mayorga1994@outlook.com>
2021-08-14 08:55:02 -0600

    fix: allow to see APIs in home controller

---
+++ jsender/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using jsender.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace jsender.API.Controllers
+{
+  
+    [Route("api/home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public ActionResult GetUrls()
+        {
+            var url = new ApiUrl()
+            {
+                Url = "messages_url",
+                Value = "api/v1/messages"
+            };
+
+            return Ok(url);
+        }
+    }
+}
---
+++ jsender/Models/ApiUrl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace jsender.API.Models
+{
+    public class ApiUrl
+    {
+        public string Url { get; set; }
+        public string Value { get; set; }
+
+        public ApiUrl()
+        {
+        }
+    }
+}
